8a3d841cfc6a527b5402f60112facd52a93ce7f9
Eriyahu1482 <124336619+Eriyahu1482@users.noreply.github.com>
2023-03-20 20:42:31 +0300

    12

--- SI-Task6/View/MainPage/MainPage1.xaml.cs
+++ SI-Task6/View/MainPage/MainPage1.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SI_Task6.View.MainPage.TaskPage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static SI_Task6.Core.Core;
 
 namespace SI_Task6.View.MainPage
 {
@@ -23,6 +25,334 @@ namespace SI_Task6.View.MainPage
         public MainPage1()
         {
             InitializeComponent();
+        }
+
+        private void BTN1_Click(object sender, RoutedEventArgs e)
+        {
+            Brd1.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN2_Click(object sender, RoutedEventArgs e)
+        {
+            Brd2.Visibility = Visibility.Visible;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+
+        }
+
+        private void BTN3_Click(object sender, RoutedEventArgs e)
+        {
+            Brd3.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN4_Click(object sender, RoutedEventArgs e)
+        {
+            Brd4.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN5_Click(object sender, RoutedEventArgs e)
+        {
+            Brd5.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN6_Click(object sender, RoutedEventArgs e)
+        {
+            Brd6.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN7_Click(object sender, RoutedEventArgs e)
+        {
+            Brd7.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN8_Click(object sender, RoutedEventArgs e)
+        {
+            Brd8.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN9_Click(object sender, RoutedEventArgs e)
+        {
+            Brd9.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN10_Click(object sender, RoutedEventArgs e)
+        {
+            Brd10.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN11_Click(object sender, RoutedEventArgs e)
+        {
+            Brd11.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN12_Click(object sender, RoutedEventArgs e)
+        {
+            Brd12.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN13_Click(object sender, RoutedEventArgs e)
+        {
+            Brd13.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+            Brd14.Visibility = Visibility.Hidden;
+        }
+
+        private void BTN14_Click(object sender, RoutedEventArgs e)
+        {
+            Brd14.Visibility = Visibility.Visible;
+            Brd2.Visibility = Visibility.Hidden;
+            Brd1.Visibility = Visibility.Hidden;
+            Brd4.Visibility = Visibility.Hidden;
+            Brd5.Visibility = Visibility.Hidden;
+            Brd6.Visibility = Visibility.Hidden;
+            Brd7.Visibility = Visibility.Hidden;
+            Brd8.Visibility = Visibility.Hidden;
+            Brd9.Visibility = Visibility.Hidden;
+            Brd10.Visibility = Visibility.Hidden;
+            Brd11.Visibility = Visibility.Hidden;
+            Brd12.Visibility = Visibility.Hidden;
+            Brd13.Visibility = Visibility.Hidden;
+            Brd3.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            CoreNavigate.MyCore?.Navigate(new MainPage());
+        }
+
+        private void BtnAnswer2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer11_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer12_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer13_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer14_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnAnswer1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
